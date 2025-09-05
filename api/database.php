69c59fe35a2767fceaf188f8c4fa1e0c3c3fab27
mario-leandro <marioleandro2004@gmail.com>
2025-09-05 11:55:16 -0300

    Criando conexão com o banco de dados

---
+++ api/database.php
@@ -0,0 +1,16 @@
+<?php
+// Configurações de conexão com o banco de dados
+$servername = 'localhost';
+$username = 'root';
+$password = '#Mario2004';
+$dbname = 'cep_db';
+
+// Cria a conexão
+try {
+    $db_connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Define o modo de erro para lançar exceções
+    $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conectado com sucesso";
+} catch(PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
